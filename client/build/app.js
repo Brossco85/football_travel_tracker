256cbc9a814f875185f67975ba2b6113cfaf8cd7
Bobby Ross <bobby1985uk@gmail.com>
2016-12-15 17:33:30 +0000

    updated app.js

--- client/build/app.js
+++ client/build/app.js
@@ -1,6 +1,6 @@
 window.onload = function() {
   var container = document.getElementById('map');
   var coords = {lat: 51.6032, lng: 0.0657};  
-  var mainMap = new MapWrapper(container, coords, 9);
+  var mainMap = new MapWrapper(container, coords, 6);
   mainMap.setLocation(coords);
 }
