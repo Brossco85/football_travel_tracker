5fe10be981e8b957bfc305c9ba8e39a8b1cd649d
Bobby Ross <bobby1985uk@gmail.com>
2016-12-15 17:10:06 +0000

    added icon to map

--- client/build/app.js
+++ client/build/app.js
@@ -2,4 +2,5 @@ window.onload = function() {
   var container = document.getElementById('map');
   var coords = {lat: 51.6032, lng: 0.0657};  
   var mainMap = new MapWrapper(container, coords, 9);
+  mainMap.setLocation(coords);
 }
