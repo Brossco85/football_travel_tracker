d61b8edce4fbd72a7622915ff9e6474da149ab87
Bobby Ross <bobby1985uk@gmail.com>
2016-12-16 22:45:02 +0000

    all stadiums now showing on map

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var fs = require('fs');
+
+
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
+
+var ACCOUNTS_FILE = path.join(__dirname + '/stadi_api.json');
+
+app.get('/api/accounts', function(req,res) {
+  fs.readFile(ACCOUNTS_FILE, function(err, data){
+    if(err){
+      console.error(err)
+      return
+    }
+    res.json(JSON.parse(data))
+  })
+})
 
 app.use(express.static('client/build'));
 
@@ -15,3 +30,8 @@ var server = app.listen(3000, function () {
 
   console.log('Example app listening at http://%s:%s', host, port);
 });
+
+// var fs = require('fs');
+// var ACCOUNTS_FILE = path.join(__dirname + '/sample.json');
+//...
+
