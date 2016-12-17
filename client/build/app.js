f4ede0a13c2619aad6c1ebe21a8a1fd715a362ec
Bobby Ross <bobby1985uk@gmail.com>
2016-12-17 20:03:37 +0000

    directions now linked to dropdown of pl teams

--- client/build/app.js
+++ client/build/app.js
@@ -13,31 +13,31 @@ window.onload = function() {
   var url = 'http://localhost:3000/api/accounts';
   makeRequest(url, function(){
     if (this.status !== 200) return;
-  var jsonString = this.responseText;
-  var stadiums = JSON.parse(jsonString);
-  var allStadiums = getStadiumData(stadiums);
-  getClubNames(allStadiums);
+    var jsonString = this.responseText;
+    var stadiums = JSON.parse(jsonString);
+    var allStadiums = getStadiumData(stadiums);
+    getClubNames(allStadiums);
 
-  for(var stadium of allStadiums){
-    var icon = {
+    for(var stadium of allStadiums){
+      var icon = {
               url: stadium.crest, // url
               scaledSize: new google.maps.Size(20, 30), // scaled size
               origin: new google.maps.Point(0,0), // origin
               anchor: new google.maps.Point(0, 0) // anchor
             };
-    mainMap.addMarker(stadium.latlng, icon);
-  };
+            mainMap.addMarker(stadium.latlng, icon);
+          };
 
-    })
+        })
 
-  mainMap.initDirections();
+  // mainMap.initDirections();
 
   var select = document.querySelector('#team');
   select.addEventListener('change', function(e){getStadiumCoords(e.target.value, mainMap)
-    })
+  })
 
 
-  }
+}
 
 
 var makeRequest = function(url, callback){
@@ -62,7 +62,7 @@ var getStadiumData = function(stadiums){
     data.push(stadiData);
 
   })
-    return data;
+  return data;
 }
 
 var getClubNames = function(stadiums){
@@ -71,23 +71,29 @@ var getClubNames = function(stadiums){
     var option = document.createElement('option') ;
     option.innerText = stadiums[i].name;
     option.value = i;
-    console.log(option.value)
+    // console.log(option.value)
     select.appendChild(option); 
   }
 }
 
-  var getStadiumCoords = function(index, map){
-    var url = 'http://localhost:3000/api/accounts';
-    makeRequest(url, function(){
-      if (this.status !== 200) return;
+var getStadiumCoords = function(index, map){
+  var url = 'http://localhost:3000/api/accounts';
+  makeRequest(url, function(){
+    if (this.status !== 200) return;
     var jsonString = this.responseText;
     var stadiums = JSON.parse(jsonString);
     var allStadiums = getStadiumData(stadiums);
-    console.log(allStadiums[index].latlng.lat)
+    // console.log(allStadiums[index].latlng.lat)
     var coords = {};
     coords = {lat: allStadiums[index].latlng.lat, lng: allStadiums[index].latlng.lng};
     map.setCenter(coords);
-    map.satelliteCloseUp();
+    // map.satelliteCloseUp();
+    var origin = {lat: 51.6032, lng: 0.0657};
+    var destination = coords;
+    map.initDirections(origin, destination);
+
+
+
   })
 }
 
--- client/build/mapWrapper.js
+++ client/build/mapWrapper.js
@@ -27,12 +27,13 @@ MapWrapper.prototype = {
    this.addMarker(coords);
    this.setCenter(coords);
  },
- initDirections: function(){
+ initDirections: function(origin, destination){
+  
   var markerArray = [];
   var directionsService = new google.maps.DirectionsService;
   var directionsDisplay = new google.maps.DirectionsRenderer({map: this.googleMap});
   var stepDisplay = new google.maps.InfoWindow;
-  calculateAndDisplayRoute(directionsDisplay, directionsService, markerArray, stepDisplay, this.googleMap);
+  calculateAndDisplayRoute(directionsDisplay, directionsService, markerArray, stepDisplay, this.googleMap, origin, destination);
 
 // var onChangeHandler = function() {
 //   calculateAndDisplayRoute(directionsDisplay, directionsService, markerArray, stepDisplay, map);
@@ -49,18 +50,23 @@ satelliteCloseUp: function(){
 }
 
 
-function calculateAndDisplayRoute(directionsDisplay, directionsService, markerArray, stepDisplay, map) {
-  for (var i = 0; i < markerArray.length; i++) {
-   markerArray[i].setMap(null);
- }
- directionsService.route({origin: {lat: 51.6032, lng: 0.0657}, destination: {lat: 53.6032, lng: 0.0657},
+function calculateAndDisplayRoute(directionsDisplay, directionsService, markerArray, stepDisplay, map, origin, destination) {
+    // directionsDisplay.setMap(null);
+
+  // if(directionsDisplay){
+  //             directionsDisplay.setMap(null);
+          // }
+ //  for (var i = 0; i < markerArray.length; i++) {
+ //   markerArray[i].setMap(null);
+ // }
+ directionsService.route({origin: origin, destination: destination,
    travelMode: 'DRIVING'
  }, function(response, status) {
   if (status === 'OK') {
     document.getElementById('warnings-panel').innerHTML =
     '<b>' + response.routes[0].warnings + '</b>';
     directionsDisplay.setDirections(response);
-    showSteps(response, markerArray, stepDisplay, map);
+    // showSteps(response, markerArray, stepDisplay, map);
   } else {
     window.alert('Directions request failed due to ' + status);
   }
