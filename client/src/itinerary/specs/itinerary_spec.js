8c8ef9b804b77a2b64d54fe8d3bd733fb115b703
Bobby Ross <bobby1985uk@gmail.com>
2016-12-19 15:45:57 +0000

    added before each to itinerary spec

--- client/src/itinerary/specs/itinerary_spec.js
+++ client/src/itinerary/specs/itinerary_spec.js
@@ -1,9 +1,15 @@
 var Itinerary = require('../itinerary');
 var assert = require('assert');
 
+var itinerary;
+
 describe('itinerary', function() {
+
+  beforeEach(function(){
+    itinerary = new Itinerary({user:'Bobby', match:"Sat Dec 17 2016 Crystal Palace FC vs Chelsea FC 12:30", startTime: "10:30", pubs: [], eateries: [], hotels: []});
+  });
+
   it('should have user', function() {
-    var itinerary = new Itinerary({user:'Bobby', match:"Sat Dec 17 2016 Crystal Palace FC vs Chelsea FC 12:30", startTime: "10:30", pubs: [], eateries: [], hotels: []});
     assert.equal(itinerary.user, 'Bobby');
   });
 
