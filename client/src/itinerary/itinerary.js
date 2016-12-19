64abfbfe458628113ef057f2c8fe727e604dbf1a
Bobby Ross <bobby1985uk@gmail.com>
2016-12-19 14:51:15 +0000

    added folders and files for itinerary and planner

---
+++ client/src/itinerary/itinerary.js
@@ -0,0 +1,8 @@
+var Itinerary = function(params){
+  this.user = params.user;
+  this.match = params.match;
+  this.startTime = params.startTime;
+  this.pubs = params.pubs;
+  this.eateries = params.eateries;
+  this.hotels = hotels;
+}
