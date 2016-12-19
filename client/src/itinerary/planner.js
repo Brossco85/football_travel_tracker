5feb48648288b922276972b98010c2b0124e2c09
Bobby Ross <bobby1985uk@gmail.com>
2016-12-19 21:27:09 +0000

    added planner constructor and basic functions

---
+++ client/src/itinerary/planner.js
@@ -0,0 +1,19 @@
+var Planner = function(){
+  this.planner = [];
+}
+
+Planner.prototype = {
+  addItinerary: function(itinerary){
+    this.planner.push(itinerary);
+  },
+  findItineraryByUser:function(user) {
+    var foundItinerary = null;
+    for (var itinerary of this.planner) {
+      if(itinerary.user === user) {
+        foundItinerary = itinerary;
+      }
+    }
+    return foundItinerary;
+  }
+
+}
