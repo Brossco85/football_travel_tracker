17399bdfa0f63229d8daf86e00b2c3cdf432c289
Bobby Ross <bobby1985uk@gmail.com>
2016-12-20 13:16:24 +0000

    added mongo functionality, returning data from db

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var fs = require('fs');
+var MongoClient = require('mongodb').MongoClient;
+
 
 
 
@@ -20,6 +22,17 @@ app.get('/api/accounts', function(req,res) {
     res.json(JSON.parse(data))
   })
 })
+
+app.get('/itineraries', function(req, res){
+ var url = 'mongodb://localhost:27017/premier_planner';
+ MongoClient.connect(url, function(err, db){
+  var collection = db.collection('planners');
+  collection.find({}).toArray(function(err, docs){
+    res.json(docs);
+    db.close();
+  })
+})
+});
 
 app.use(express.static('client/build'));
 
